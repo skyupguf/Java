0bb3cf90ce366207607ef59bfa319393ea50ea1e
skyupguf <skyupguf@gmail.com>
2022-11-06 19:00:36 +0900

    practice Java for var

---
+++ Java_01_Var/src/Var.java
@@ -0,0 +1,70 @@
+// Java 프로그래밍 - 변수
+
+public class Var {
+    public static void main(String[] args) {
+
+//      1. 변수 사용하기
+        System.out.println("== 변수 사용하기 ==");
+
+        int age = 34;
+        System.out.println(age);
+
+        String country = "Korea";
+        System.out.println(country);
+
+//      2. 변수 이름 규칙
+        System.out.println("== 변수 이름 규칙 ==");
+
+//      2-1. 문자, 숫자, _(underscore), $ 사용 가능
+        int apple = 1000;
+        int apple2 = 2000;
+        int _apple = 3000;
+        int $apple = 4000;
+        System.out.println(apple);
+        System.out.println(apple2);
+        System.out.println(_apple);
+        System.out.println("$apple = " + $apple);
+
+//      2-2. 숫자로 시작 X
+//        int 2apple = 2000;
+
+//      2-3. 대소문자 구분
+        int banana = 1000;
+        int BANANA = 2000;
+        System.out.println(banana);
+        System.out.println("BANANA = " + BANANA);
+
+//      2-4. 공백 사용 X
+//        int apple one = 2000;
+        int apple_one = 1000;
+        int appleOne = 1000;
+
+//      2-5. 예약어 사용 X
+//      예약어 예시: true, false, if, switch, for, continue, break, ...
+//        int if = 1000;
+
+//      참고) 한글 사용 가능
+        int 사과 = 1000;
+        System.out.println(사과);
+
+//      3. 표기법
+
+//      3-1. 카멜 표기법 (camelCase)
+//      변수, 함수
+        int myAge = 34;
+        String myName = "Ha";
+        System.out.println(myAge);
+        System.out.println(myName);
+
+//      3-2. 파스칼 표기법 (PascalCase)
+//      클래스
+        int MyAge = 34;
+        String MyName = "Ha";
+
+//      참고) 스네이크 표기법 (snake_case)
+//      사용 X
+        int my_age = 34;
+        String my_name = "Ha";
+
+    }
+}
