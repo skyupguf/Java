60b220359412579da0745a6765928c2414e14c66
skyupguf <skyupguf@gmail.com>
2022-11-10 15:05:12 +0900

    practice Java for file I.O

--- Java_13_I.O/src/Practice.java
+++ Java_13_I.O/src/Practice.java
@@ -13,10 +13,24 @@ public class Practice {
         String outputFile = "./JamesArthurGosling_edit.txt";
 
         // 찾을 단어 / 변경 단어 입력 받기
-
+        System.out.println("찾는 단어 : ");
+        Scanner sc = new Scanner(System.in);
+        String find = sc.nextLine();
+        System.out.println("변경 단어 : ");
+        String to = sc.nextLine();
 
         // 파일 읽기, 변경 및 저장
+        BufferedReader br = new BufferedReader(new FileReader(inputFile));
+        FileWriter fw = new FileWriter(outputFile);
 
+        while (true) {
+            String line = br.readLine();
+            if (line == null) break;
+            String newLine = line.replace(find, to);
+            fw.write(newLine + "\n");
+        }
 
+        br.close();
+        fw.close();
     }
 }
