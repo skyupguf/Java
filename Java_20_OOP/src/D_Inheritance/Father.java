39db76d9ed1b9ec0e0fa20390ba3d397ed9edbdd
skyupguf <skyupguf@gmail.com>
2023-01-20 02:42:25 +0900

    Learn OOP_Inheritance_extends

---
+++ Java_20_OOP/src/D_Inheritance/Father.java
@@ -0,0 +1,11 @@
+package D_Inheritance;
+
+class Father {
+}
+
+class Mother {
+}
+
+//  A-4. Java 는 다중 상속을 할 수 없다. C++과 Python 과 같은 언어는 다중 상속을 지원한다, Java 는 이를 Interface 를 활용해 구현힌다.
+//class child extends Father, Mother {
+//}
